6823e557fbbfb0ccd23935deeed587c88a99506d
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2022-09-29 10:58:48 +0530

    constants file added

---
+++ lib/constants/color_palette.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+class ColorPalette {
+  ColorPalette();
+
+  // the main green used in buttons and stuff
+  final Color primaryGreen = const Color(0xff53AE51);
+
+  // the background color 
+  final Color backgroundColor = const Color(0xffF8F8F8);
+
+  // the main title color
+  final Color mainTitleColor = const Color(0xff2A2426);
+
+  // container fill grey
+  final Color containerFillGrey = const Color(0xffF3F3F3);
+
+  // touch blue
+  final Color touchBlue = const Color(0xffE2F0F0);
+}
