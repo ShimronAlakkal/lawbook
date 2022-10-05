6efe57f1d24b4e9f699dac1defa8699c7763708c
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2022-10-05 13:12:31 +0530

    UI updt.Bug in hearingDesc selection

--- lib/views/UI/view_case.dart
+++ lib/views/UI/view_case.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 class CaseView extends StatefulWidget {
   const CaseView({super.key});
@@ -8,17 +9,38 @@ class CaseView extends StatefulWidget {
 }
 
 class _CaseViewState extends State<CaseView> {
+  bool isBannerAdLoaded = false;
+  late BannerAd _bannerAd;
+
+  @override
+  void initState() {
+    _bannerAd = BannerAd(
+        size: AdSize.banner,
+        adUnitId: 'ca-app-pub-7467125766317541/9930857264',
+        listener: BannerAdListener(
+          onAdLoaded: (ad) {
+            setState(() {
+              isBannerAdLoaded = true;
+            });
+          },
+        ),
+        request: const AdRequest())
+      ..load();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: List.generate(
-        10,
-        (index) => ListTile(
-          title: Text(
-            index.toString(),
-          ),
-        ),
-      ),
-    );
+    return ListView(children: [
+      isBannerAdLoaded
+          ? SizedBox(
+              child: AdWidget(ad: _bannerAd),
+            )
+          : SizedBox(
+              child: Center(
+                child: Text('ADs on load'),
+              ),
+            )
+    ]);
   }
 }
