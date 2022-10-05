f0505bc5daba7815317718e4e3db0472b0a181f0
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2022-10-05 13:11:42 +0530

    new colors added to palette

--- lib/constants/color_palette.dart
+++ lib/constants/color_palette.dart
@@ -6,12 +6,11 @@ class ColorPalette {
   // the main green used in buttons and stuff
   final Color primaryGreen = const Color(0xff109566);
 
-  // the secondary green 
+  // the secondary green
   final Color secondaryGreen = const Color(0xff139768);
 
   // accent green
   final Color accentGreen = const Color(0xffCCF9D8);
-  
 
   // the background color
   final Color backgroundColor = const Color(0xffF8F8F8);
@@ -24,5 +23,7 @@ class ColorPalette {
 
   // touch blue
   final Color touchBlue = const Color(0xffE2F0F0);
-}
 
+  // The blue used in clickable links in the UI
+  final Color linkBlue = const Color(0xff1976D2);
+}
