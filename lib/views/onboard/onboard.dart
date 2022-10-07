47a2596588e3dbcbaf40fbad682c0587518bae8c
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2022-10-07 20:34:13 +0530

    made onboards page, incomplete

---
+++ lib/views/onboard/onboard.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Onboard extends StatelessWidget {
+  const Onboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView(
+          children: [
+            //
+          ],
+        ),
+      ),
+    );
+  }
+}
