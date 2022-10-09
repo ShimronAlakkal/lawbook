f7dd0a63e961a1b53e2b9f2d9bb920f4df1b9388
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2022-10-09 11:27:41 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:lawbook/home_view.dart';
+import 'package:lawbook/views/onboard/onboard.dart';
 import 'package:lawbook/views/onboard/sign_in.dart';
+import 'package:lawbook/views/onboard/sign_up.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,7 +16,7 @@ Future main() async {
     const MaterialApp(
       title: 'LawBook',
       debugShowCheckedModeBanner: false,
-      home: SignIn(),
+      home: Onboard(),
     ),
   );
 }
