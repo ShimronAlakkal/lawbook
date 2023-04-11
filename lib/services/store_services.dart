4138f492b6bacaf1f368ac5424a64eddcefaee9d
ShimronAlakkal <shimron.alakkal1804@gmail.com>
2023-04-11 11:45:23 -0400

    store service for CRUD

---
+++ lib/services/store_services.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class StoreServices {
+  final FirebaseAuth _authInstance = FirebaseAuth.instance;
+
+setData({required FileMode file}) async{
+
+try {
+  
+} catch (e) {
+  
+}
+
+
+}
+
+}
